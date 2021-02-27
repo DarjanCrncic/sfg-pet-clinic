107347b0c7cd6efd1f0347522d9032fd5d014f50
DarjanCrncic <darjancr@gmail.com>
2021-02-27 10:47:11 +0100

    Added data in dataLoader, added some entities, petType service etc. Added not implemented page for non implemented links.

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/services/map/VetServiceMap.java
@@ -4,11 +4,19 @@ import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
+import com.example.sfgpetclinic.model.Speciality;
 import com.example.sfgpetclinic.model.Vet;
+import com.example.sfgpetclinic.services.SpecialityService;
 import com.example.sfgpetclinic.services.VetService;
 
 @Service
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService{
+	
+	private final SpecialityService specialityService;
+	
+	public VetServiceMap(SpecialityService specialityService) {
+		this.specialityService = specialityService;
+	}
 
 	@Override
 	public Set<Vet> findAll() {
@@ -22,6 +30,15 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetS
 
 	@Override
 	public Vet save(Vet object) {
+		
+		if(object.getSpecialities().size() > 0) {
+			object.getSpecialities().forEach(speciality->{
+				if(speciality.getId() == null) {
+					Speciality savedSpeciality = specialityService.save(speciality);
+					speciality.setId(savedSpeciality.getId());
+				}
+			});
+		}
 		return super.save(object);
 	}
 
--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,11 +1,18 @@
 package com.example.sfgpetclinic.bootstrap;
 
+import java.time.LocalDate;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import com.example.sfgpetclinic.model.Owner;
+import com.example.sfgpetclinic.model.Pet;
+import com.example.sfgpetclinic.model.PetType;
+import com.example.sfgpetclinic.model.Speciality;
 import com.example.sfgpetclinic.model.Vet;
 import com.example.sfgpetclinic.services.OwnerService;
+import com.example.sfgpetclinic.services.PetTypeService;
+import com.example.sfgpetclinic.services.SpecialityService;
 import com.example.sfgpetclinic.services.VetService;
 
 @Component
@@ -13,38 +20,92 @@ public class DataLoader implements CommandLineRunner {
 	
 	private final OwnerService ownerService;
 	private final VetService vetService;
+	private final PetTypeService petTypeService;
+	private final SpecialityService specialityService;
 
-	public DataLoader(OwnerService ownerService, VetService vetService) {
+	public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService, SpecialityService specialityService) {
 		this.ownerService = ownerService;
 		this.vetService = vetService;
+		this.petTypeService = petTypeService;
+		this.specialityService = specialityService;
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		
+		int count = petTypeService.findAll().size();
+		
+		if(count == 0) {
+			loadData();
+		}
+
+	}
+
+	private void loadData() {
+		PetType dog = new PetType();
+		dog.setName("Dog");
+		PetType savedDogPetType = petTypeService.save(dog);
+		
+		PetType cat = new PetType();
+		dog.setName("Dog");
+		PetType savedCatPetType = petTypeService.save(cat);
+		
+		Speciality radiology = new Speciality();
+		radiology.setDescription("Radiology");
+		Speciality savedRadiology = specialityService.save(radiology);
+		
+		Speciality surgery = new Speciality();
+		surgery.setDescription("Surgery");
+		Speciality savedSurgery = specialityService.save(surgery);
+		
+		Speciality dentistry = new Speciality();
+		dentistry.setDescription("Dentistry");
+		Speciality savedDentistry = specialityService.save(dentistry);
+		
 		Owner owner1 = new Owner();
 		owner1.setFirstName("Michael");
 		owner1.setLastName("Weston");
+		owner1.setAddress("bla bla Street");
+		owner1.setCity("Miami");
+		owner1.setTelephone("15156167");
+		
+		Pet mikesPet = new Pet();
+		mikesPet.setPetType(savedDogPetType);
+		mikesPet.setBirthday(LocalDate.now());
+		mikesPet.setOwner(owner1);
+		mikesPet.setName("Rosco");
+		owner1.getPets().add(mikesPet);
 		
 		ownerService.save(owner1);
 		
 		Owner owner2 = new Owner();
 		owner2.setFirstName("Fiona");
 		owner2.setLastName("Axe");
+		owner2.setAddress("123 izh");
+		owner2.setCity("Miami");
+		owner2.setTelephone("15156167");
+		
+		Pet fionasCat = new Pet();
+		fionasCat.setPetType(savedCatPetType);
+		fionasCat.setBirthday(LocalDate.now());
+		fionasCat.setOwner(owner2);
+		fionasCat.setName("Julia");
+		owner2.getPets().add(fionasCat);
 		
 		ownerService.save(owner2);
 		
 		Vet vet1 = new Vet();
         vet1.setFirstName("Sam");
         vet1.setLastName("Axe");
+        vet1.getSpecialities().add(savedRadiology);
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
         vet2.setFirstName("Jessie");
         vet2.setLastName("Porter");
+        vet2.getSpecialities().add(savedDentistry);
 
         vetService.save(vet2);
-		
 	}	
 }
